461a974f3bca2a563d972f4156eddbd9d6fb7c2c
prashasth nair <prashasthnair1@gmail.com>
2024-01-14 13:44:26 +0530

    Fixed bugs and improved wallpaper gallery UI

--- main.js
+++ main.js
@@ -1,200 +1,206 @@
-
 // Variables
-const settings_dialoge = document.querySelector('.Settings_window');
-const settings_cancel_button = document.querySelector('#settings_cancel');
-const settings_save_button = document.querySelector('#settings_save');
-const settings_button = document.querySelector('#settings');
+const settings_dialoge = document.querySelector(".Settings_window");
+const settings_cancel_button = document.querySelector("#settings_cancel");
+const settings_save_button = document.querySelector("#settings_save");
+const settings_button = document.querySelector("#settings");
 
-const search_box = document.querySelector('#Text');
-const quotes = document.querySelector('#quotes');
-const icon = document.querySelector('.icon-field');
+const search_box = document.querySelector("#Text");
+const quotes = document.querySelector("#quotes");
+const icon = document.querySelector(".icon-field");
 
-const General_tab = document.querySelector('#General_tab');
-const Search_tab = document.querySelector('#Search_tab');
+const General_tab = document.querySelector("#General_tab");
+const Search_tab = document.querySelector("#Search_tab");
 
-const General_screen = document.querySelector('.General');
-const Search_screen = document.querySelector('.Search');
-var new_tab_isChecked = localStorage.getItem('new_tab_isChecked');
+const General_screen = document.querySelector(".General");
+const Search_screen = document.querySelector(".Search");
+var new_tab_isChecked = localStorage.getItem("new_tab_isChecked");
 
-var hour_format = localStorage.getItem('hour_format');
+var hour_format = localStorage.getItem("hour_format");
 
 var url = "https://api.quotable.io/random";
 
-
-
 async function fetchDataAsync(url) {
-    const response = await fetch(url);
+  const response = await fetch(url);
 
-    const data = await response.json();
-    document.getElementById('quotes').innerHTML = "\"" + [data['content']] + "\"" + "<br><br>--" + data['author'];
+  const data = await response.json();
+  document.getElementById("quotes").innerHTML =
+    '"' + [data["content"]] + '"' + "<br><br>--" + data["author"];
 }
 fetchDataAsync(url);
 
 // Settings
 
-General_tab.style.background = '#dbdbdb';
-if (new_tab_isChecked === 'true') {
-    document.getElementById("switch").checked = true;
-}
-else {
-    document.getElementById("switch").checked = false;
+General_tab.style.background = "#dbdbdb";
+if (new_tab_isChecked === "true") {
+  document.getElementById("switch").checked = true;
+} else {
+  document.getElementById("switch").checked = false;
 }
 settings_button.addEventListener("click", () => {
-    settings_dialoge.showModal();
+  settings_dialoge.showModal();
 });
 
 // Click events
 document.getElementById("Search_button").addEventListener("click", Click);
 
 settings_cancel_button.addEventListener("click", () => {
-    settings_dialoge.close();
+  settings_dialoge.close();
 });
 
-
 settings_save_button.addEventListener("click", () => {
-    hour_format = document.getElementById('hour').value;
-    new_tab_isChecked = document.getElementById('switch').checked;
+  hour_format = document.getElementById("hour").value;
+  new_tab_isChecked = document.getElementById("switch").checked;
 
-    localStorage.setItem("hour_format", hour_format);
-    localStorage.setItem("new_tab_isChecked", new_tab_isChecked);
-    settings_dialoge.close();
-    window.location.reload();
-})
+  localStorage.setItem("hour_format", hour_format);
+  localStorage.setItem("new_tab_isChecked", new_tab_isChecked);
+  settings_dialoge.close();
+  window.location.reload();
+});
 
 // General Settings
 General_tab.addEventListener("click", () => {
-    // Button background
-    Search_tab.style.background = null;
-    General_tab.style.background = '#dbdbdb';
+  // Button background
+  Search_tab.style.background = null;
+  General_tab.style.background = "#dbdbdb";
 
-    // hide show div
-    if (General_screen.style.display === 'none') {
-        General_screen.style.display = 'block'
-    }
-    Search_screen.style.display = "none";
-})
+  // hide show div
+  if (General_screen.style.display === "none") {
+    General_screen.style.display = "block";
+  }
+  Search_screen.style.display = "none";
+});
 hour_format = JSON.stringify(hour_format);
 
 if (JSON.parse(hour_format) != null) {
-    if (JSON.parse(hour_format) == "12 hour") {
-        updateClock_12();
-        document.getElementById('hour').selectedIndex = 0;
-
-    }
-    else if (JSON.parse(hour_format) == "24 hour") {
-        updateClock_24();
-        document.getElementById('hour').selectedIndex = 1;
-    }
-}
-else {
+  if (JSON.parse(hour_format) == "12 hour") {
     updateClock_12();
+    document.getElementById("hour").selectedIndex = 0;
+  } else if (JSON.parse(hour_format) == "24 hour") {
+    updateClock_24();
+    document.getElementById("hour").selectedIndex = 1;
+  }
+} else {
+  updateClock_12();
 }
 
 Search_tab.addEventListener("click", () => {
-    General_tab.style.background = null;
-    Search_tab.style.background = "#dbdbdb";
-    // hide show div
-    Search_screen.style.display = 'block'
+  General_tab.style.background = null;
+  Search_tab.style.background = "#dbdbdb";
+  // hide show div
+  Search_screen.style.display = "block";
 
-
-    General_screen.style.display = "none";
-})
+  General_screen.style.display = "none";
+});
 
 updateDate();
 
 // Function to get time
 function updateClock_24() {
-    var now = new Date(); // current date
-    if (now.getMinutes() < 10) {
-        time = now.getHours() + ':0' + now.getMinutes();
-    }
-    else {
-        time = now.getHours() + ':' + now.getMinutes();
-    }
+  var now = new Date(); // current date
+  if (now.getMinutes() < 10) {
+    time = now.getHours() + ":0" + now.getMinutes();
+  } else {
+    time = now.getHours() + ":" + now.getMinutes();
+  }
 
-    // set the content of the element with the ID time to the formatted string
-    document.getElementById('time').innerHTML = [time];
+  // set the content of the element with the ID time to the formatted string
+  document.getElementById("time").innerHTML = [time];
 
-    // call this function again in 1000ms
-    setTimeout(updateClock_24, 1000);
+  // call this function again in 1000ms
+  setTimeout(updateClock_24, 1000);
 }
 
 function updateClock_12() {
-    var now = new Date(); // current date
-    var min, hr;
-    if (now.getMinutes() < 10) {
-        min = '0' + now.getMinutes()
-    }
-    else {
-        min = now.getMinutes();
-    }
-    if (now.getHours() > 12) {
-        hr = now.getHours() - 12;
-        time = hr + ':' + min + " PM";
-    }
-    else {
-        hr = now.getHours();
-        time = hr + ':' + min + " AM";
-    }
+  var now = new Date(); // current date
+  var min, hr;
+  if (now.getMinutes() < 10) {
+    min = "0" + now.getMinutes();
+  } else {
+    min = now.getMinutes();
+  }
+  if (now.getHours() > 12) {
+    hr = now.getHours() - 12;
+    time = hr + ":" + min + " PM";
+  } else {
+    hr = now.getHours();
+    time = hr + ":" + min + " AM";
+  }
 
-    // set the content of the element with the ID time to the formatted string
-    document.getElementById('time').innerHTML = [time];
+  // set the content of the element with the ID time to the formatted string
+  document.getElementById("time").innerHTML = [time];
 
-    // call this function again in 1000ms
-    setTimeout(updateClock_12, 1000);
+  // call this function again in 1000ms
+  setTimeout(updateClock_12, 1000);
 }
 
 function updateDate() {
-    var now = new Date(), // current date
-        months = ['January', 'February', 'March', 'April', 'May', 'June',
-            'July', 'August', 'September', 'October', 'November', 'December'],
-        days = ["Sunday", "Monday", "Tuesday", "Wednesday",
-            "Thursday", "Friday", "Saturday"];
-    // you get the idea
+  var now = new Date(), // current date
+    months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ],
+    days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+  // you get the idea
 
+  // a cleaner way than string concatenation
+  date = [months[now.getMonth()], now.getDate()].join(" ");
+  day_date = [days[now.getDay()], date].join(", ");
 
-    // a cleaner way than string concatenation
-    date = [months[now.getMonth()],
-    now.getDate()].join(" ");
-    day_date = [days[now.getDay()],
-        date].join(', ');
+  // set the content of the element with the ID time to the formatted string
+  document.getElementById("day_date").innerHTML = [day_date];
 
-
-    // set the content of the element with the ID time to the formatted string
-    document.getElementById('day_date').innerHTML = [day_date];
-
-    // call this function again in 1000ms
-    setTimeout(updateDate, 1000);
+  // call this function again in 1000ms
+  setTimeout(updateDate, 1000);
 }
 
 if (performance.navigation.type == performance.navigation.TYPE_BACK_FORWARD) {
-    document.getElementById('Text').value = "";
+  document.getElementById("Text").value = "";
 }
 // TODO: Enter key
-document.getElementById('Form').onsubmit = function () {
-    // Enter key will search
-    // process
-    // Click()
-    return false;
-}
+document.getElementById("Form").onsubmit = function () {
+  // Enter key will search
+  // process
+  // Click()
+  return false;
+};
 
 function suggest_show() {
-    document.getElementById('Text').style.borderRadius = '15px 0px 0px 0px';
-    document.getElementById('Search_button').style.borderRadius = '0px 15px 0px 0px';
-    document.querySelector("#suggestion_list").style.display = 'block';
+  document.getElementById("Text").style.borderRadius = "15px 0px 0px 0px";
+  document.getElementById("Search_button").style.borderRadius =
+    "0px 15px 0px 0px";
+  document.querySelector("#suggestion_list").style.display = "block";
 }
 function suggest_hide() {
-    document.getElementById('Text').style.borderRadius = '15px 0px 0px 15px';
-    document.getElementById('Search_button').style.borderRadius = '0px 15px 15px 0px';
-    document.querySelector("#suggestion_list").style.display = 'none';
+  document.getElementById("Text").style.borderRadius = "15px 0px 0px 15px";
+  document.getElementById("Search_button").style.borderRadius =
+    "0px 15px 15px 0px";
+  document.querySelector("#suggestion_list").style.display = "none";
 }
 
 function Click() {
-    var query = 'http://google.com/search?q=' + document.getElementById('Text').value;
-    if (new_tab_isChecked === 'true') {
-        window.open(query);//opens page in new tab
-    } else {
-
-        window.open(query, "_self");//opens page in same tab
-    }
+  var query =
+    "http://google.com/search?q=" + document.getElementById("Text").value;
+  if (new_tab_isChecked === "true") {
+    window.open(query); //opens page in new tab
+  } else {
+    window.open(query, "_self"); //opens page in same tab
+  }
 }
--- resource/wallpaper.js
+++ resource/wallpaper.js
@@ -1,117 +1,148 @@
+const Wallpaper_dialoge = document.querySelector(".Wallpaper_window");
+const grid = document.querySelector(".grid");
+const Wallpaper_upload = document.querySelector(".wallpaper_upload");
+const Wallpaper_close = document.querySelector("#close");
+const wallpaper_button = document.querySelector("#wallpaper");
+const Wallpaper_upload_button = document.querySelector(
+  "#Wallpaper_user_upload"
+);
+const Wallpaper_Home_button = document.querySelector("#Wallpaper_Home");
 
-const Wallpaper_dialoge = document.querySelector('.Wallpaper_window');
-const Wallpaper_main = document.querySelector('.wallpaper_main');
-const Wallpaper_upload = document.querySelector('.wallpaper_upload');
-const Wallpaper_close = document.querySelector('#close');
-const wallpaper_button = document.querySelector('#wallpaper');
-const Wallpaper_upload_button = document.querySelector('#Wallpaper_user_upload');
-const Wallpaper_Home_button = document.querySelector('#Wallpaper_Home');
-
-var bgimage = localStorage.getItem('wallpaper_img');
+var bgimage = localStorage.getItem("wallpaper_img");
 let is_home = true;
 
-if(is_home){
-    Wallpaper_Home_button.style.color = "#42a5f5";
-    Wallpaper_upload_button.style.color = "black";
-}else{
-    Wallpaper_upload_button.style.color = "#42a5f5";
-    Wallpaper_Home_button.style.color = "black";
+if (is_home) {
+  Wallpaper_Home_button.style.color = "#42a5f5";
+  Wallpaper_upload_button.style.color = "white";
+} else {
+  Wallpaper_upload_button.style.color = "#42a5f5";
+  Wallpaper_Home_button.style.color = "white";
 }
 
-// wallpaper
+// default wallpaper
 if (bgimage != null) {
-    document.body.style.backgroundImage = "url(" + bgimage + ")";
-}else{
-    document.body.style.backgroundImage = "url('wallpapers/Wallpaper.jpg')";
+  document.body.style.backgroundImage = "url(" + bgimage + ")";
+} else {
+  document.body.style.backgroundImage = "url('wallpapers/Wallpaper.jpg')";
 }
 
-
-
-const numImagesAvailable = 988  //how many photos are total in the collection
+const numImagesAvailable = 988; //how many photos are total in the collection
 const numItemsToGenerate = 20; //how many photos you want to display
-const imageWidth = 480;    //image width in pixels
-const imageHeight = 480;   //image height in pixels
-const collectionID = 928423  //Beach & Coastal, the collection ID from the original url
-var wallpaper_lis = [];
-function renderGalleryItem(randomNumber){
-  fetch(`https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?sig=${randomNumber}`)
-    .then((response) => {
-        wallpaper_lis.push(response.url)
-       
-  })
-}
-for(let i=0; i < numItemsToGenerate; i++){
-    let randomImageIndex = Math.floor(Math.random() * numImagesAvailable);
-    renderGalleryItem(randomImageIndex);
-}
-console.log(wallpaper_lis)
-
+const imageWidth = 1920; //image width in pixels
+const imageHeight = 1080; //image height in pixels
+const collectionID = 928423; //Beach & Coastal, the collection ID from the original url
 // var wallpaper_lis = ["wallpapers\\Wallpaper.jpg", "wallpapers\\Wallpaper1.jpg", "wallpapers\\Wallpaper2.jpg", "wallpapers\\Wallpaper3.jpg"];
 
-var mosaic = new mosaicLayout({
-    imagesArray: wallpaper_lis,
-    lazyLoading: true,
-    lazyLoadingClass: 'lazy',
-    columns: 4,
-    
-});
-mosaic.initiate();
-mosaic.loadImages();
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
+var wallpaper_lis = [];
 
-        reader.onload = function (e) {
-            document.getElementById('Display_img').setAttribute('src', e.target.result);
-        };
-        console.log(input.files[0])
-
-        reader.readAsDataURL(input.files[0]);
-    }
+function getImageURLs(randomNumber) {
+  return new Promise((resolve, reject) => {
+    fetch(
+      `https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?sig=${randomNumber}`
+    )
+      .then((response) => {
+        wallpaper_lis.push(response.url);
+        resolve();
+      })
+      .catch(reject);
+  });
 }
-Wallpaper_upload_button.addEventListener('click', () => {
-    Wallpaper_main.style.display = 'none';
-    Wallpaper_upload.style.display = 'block';
-    Wallpaper_upload_button.style.color = "#42a5f5";
-    Wallpaper_Home_button.style.color = "black";
 
-})
+// Generate random image URLs
+let promises = [];
+for (let i = 0; i < numItemsToGenerate; i++) {
+  let randomImageIndex = Math.floor(Math.random() * numImagesAvailable);
+  promises.push(getImageURLs(randomImageIndex));
+}
 
-Wallpaper_Home_button.addEventListener('click', () => {
-    Wallpaper_main.style.display = 'flex';
-    Wallpaper_upload.style.display = 'none';
-    Wallpaper_Home_button.style.color = "#42a5f5";
-    Wallpaper_upload_button.style.color = "black";
-})
-document.getElementById('save_upload').addEventListener('click', () => {
-    const reader = new FileReader();
+// Wait for all promises to be resolved
+Promise.all(promises)
+  .then(() => {
+    // Now check the length of the array
+    loadAndArrangeImages();
+  })
+  .catch((error) => {
+    console.error("Error fetching images:", error);
+  });
 
-    let files = document.getElementById('image-file').files
-    reader.onload = async (event) => {
-        document.body.style.backgroundImage = "url(" + event.target.result + ")";
-        localStorage.setItem("wallpaper_img", event.target.result);
-    }
-    reader.readAsDataURL(files[0])
+// Function to load and arrange images using Masonry
+function loadAndArrangeImages() {
+  const imageContainer = document.getElementById("imageContainer");
 
-})
-document.getElementById('image-file').addEventListener('change', (event) => {
-    readURL(document.getElementById('image-file'));
-})
-document.getElementById('masonryContainer').addEventListener('click', (e) => {
-    var bgimage = document.getElementById(e.target.id).src;
-    if (bgimage) {
-        document.body.style.backgroundImage = "url(" + bgimage + ")";
-        localStorage.setItem("wallpaper_img", bgimage);
-        console.log('id', bgimage)
-    }
-})
+  wallpaper_lis.forEach((url) => {
+    const img = new Image();
+    img.src = url;
+    img.classList.add("grid-item");
 
+    imageContainer.appendChild(img);
+  });
+  // Use imagesLoaded to ensure all images are loaded before initializing Masonry
+  imagesLoaded(imageContainer, function () {
+    // Initialize Masonry
+    // Initialize Masonry
+    const masonry = new Masonry(".grid", {
+      itemSelector: "grid-item",
+      columnWidth: 200, // Adjust as needed
+      gutter: 10, // Adjust as needed
+      fitWidth: true,
+    });
+  });
+}
+
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      document
+        .getElementById("Display_img")
+        .setAttribute("src", e.target.result);
+    };
+    console.log(input.files[0]);
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+Wallpaper_upload_button.addEventListener("click", () => {
+  grid.style.display = "none";
+  Wallpaper_upload.style.display = "block";
+  Wallpaper_upload_button.style.color = "#42a5f5";
+  Wallpaper_Home_button.style.color = "white";
+});
+
+Wallpaper_Home_button.addEventListener("click", () => {
+  grid.style.display = "grid";
+  Wallpaper_upload.style.display = "none";
+  Wallpaper_Home_button.style.color = "#42a5f5";
+  Wallpaper_upload_button.style.color = "white";
+});
+document.getElementById("save_upload").addEventListener("click", () => {
+  const reader = new FileReader();
+
+  let files = document.getElementById("image-file").files;
+  reader.onload = async (event) => {
+    document.body.style.backgroundImage = "url(" + event.target.result + ")";
+    localStorage.setItem("wallpaper_img", event.target.result);
+  };
+  reader.readAsDataURL(files[0]);
+});
+document.getElementById("image-file").addEventListener("change", (event) => {
+  readURL(document.getElementById("image-file"));
+});
+
+document.getElementById("imageContainer").addEventListener("click", (e) => {
+  var bgimage = document.getElementById(e.target.id).src;
+  if (bgimage) {
+    document.body.style.backgroundImage = "url(" + bgimage + ")";
+    localStorage.setItem("wallpaper_img", bgimage);
+    console.log("id", bgimage);
+  }
+});
 
 wallpaper_button.addEventListener("click", () => {
-
-    Wallpaper_dialoge.showModal();
-
+  Wallpaper_dialoge.showModal();
 });
 Wallpaper_close.addEventListener("click", () => {
-    Wallpaper_dialoge.close();
+  Wallpaper_dialoge.close();
 });
